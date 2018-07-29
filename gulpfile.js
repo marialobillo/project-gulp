68e3b05a78fb3cb8c378f904348413bb3498ee6c
marialobillo <maria.lobillo.santos@gmail.com>
2018-07-29 10:37:24 +0200

    adding sass plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 let gulp = require('gulp')
+let sass = require('gulp-sass')
 
 gulp.task('hello', function () {
   console.log('Hello Maria')
+})
+
+gulp.task('greeting', function () {
+  return gulp.src('source-files')
+    .pipe(sass())
+    .pipe(gulp.dest('destination'))
 })
