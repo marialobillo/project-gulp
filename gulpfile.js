9d13e82ba3c5d1873504308136e1acbdde36a556
marialobillo <maria.lobillo.santos@gmail.com>
2018-07-29 11:06:22 +0200

    more gulp tasks with sass plugin

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ gulp.task('hello', function () {
   console.log('Hello Maria')
 })
 
-gulp.task('greeting', function () {
-  return gulp.src('source-files')
+gulp.task('sass', function () {
+  return gulp.src('app/scss/styles.scss')
     .pipe(sass())
-    .pipe(gulp.dest('destination'))
+    .pipe(gulp.dest('app/css'))
+})
+
+gulp.task('sass2', function () {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
 })
